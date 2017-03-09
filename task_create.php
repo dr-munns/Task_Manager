044456ffc4389ecd68edf9a107678b4f12d69bf4
dr-munns <kaspermunns@gmail.com>
2017-03-09 10:20:01 +0000

    placeholder file for task creation

---
+++ task_create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Hello, this will hold the modules for creating tasks and projects";
+
+        ?>
+
+    </body>
+</html>
