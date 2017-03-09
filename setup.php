3defc13cae0a9cdf257a681e6d2642ad352cc8f8
dr-munns <kaspermunns@gmail.com>
2017-03-09 10:16:34 +0000

    create file "setup"
    a placeholder

---
+++ setup.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Hello, this will setup Dr Munnns' task manager system";
+
+        ?>
+
+    </body>
+</html>
