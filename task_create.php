d06c4a217b4cf4094fdb3bb4eaab0c13bbcd89cf
dr-munns <kaspermunns@gmail.com>
2017-03-09 10:46:57 +0000

    start of module structure
    includes first guess at DB calling

--- task_create.php
+++ task_create.php
@@ -8,6 +8,25 @@
         <?php
             echo "Hello, this will hold the modules for creating tasks and projects";
 
+            // connect to SQL SoapServer
+
+            //- check connection and DB structure
+
+            // Load current streams, substreams and tags
+            //$current_streams = SELECT COUNT (DISTINCT stream) FROM task_info
+
+
+            //create stream
+
+
+            //create substream
+
+            //create task
+
+
+
+
+
         ?>
 
     </body>
