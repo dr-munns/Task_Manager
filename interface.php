4f05e96cad17fd9f5477e21febf58ce8feb61b44
dr-munns <kaspermunns@gmail.com>
2017-03-14 22:39:26 +0000

    even easier check

--- interface.php
+++ interface.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   //pull streams
-  $streams_all = $_SESSION['streams'];
+  //$streams_all = $_SESSION['streams'];
   //$streams = $streams_all[0];
   //$sub_1 = $streams_all[1];
   //$sub_2 = $streams_all[2];
@@ -9,6 +9,7 @@
 <!DOCTYPE HTML>
   <html>
     <body>
+
       <form method="post" action="set_active.php" >
       <br><input type="submit" name="stream" value = "work">
       <input type="submit" name="stream" value="play"><br>
