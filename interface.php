0b532c123ee943de717f375fe0b00193b6a4574e
dr-munns <kaspermunns@gmail.com>
2017-03-14 22:43:13 +0000

    switch to php

--- interface.php
+++ interface.php
@@ -2,7 +2,7 @@
   session_start();
   //pull streams
   $streams_all = $_SESSION['streams'];
-  var_dump($streams_all);
+  //var_dump($streams_all);
   //$streams = $streams_all[0];
   //$sub_1 = $streams_all[1];
   //$sub_2 = $streams_all[2];
@@ -10,11 +10,12 @@
 <!DOCTYPE HTML>
   <html>
     <body>
+      <?php
+        <form method="post" action="set_active.php" >
+        <br><input type="submit" name="stream" value = "work">
+        <input type="submit" name="stream" value="play"><br>
 
-      <form method="post" action="set_active.php" >
-      <br><input type="submit" name="stream" value = "work">
-      <input type="submit" name="stream" value="play"><br>
-
-      </form>
+        </form>
+      ?>
     </body>
   </html>
