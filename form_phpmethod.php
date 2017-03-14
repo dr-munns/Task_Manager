6910601228f626d76ed495cc001d72792e767ee9
dr-munns <kaspermunns@gmail.com>
2017-03-14 23:27:00 +0000

    real test now

--- form_phpmethod.php
+++ form_phpmethod.php
@@ -1,15 +1,15 @@
 <?php
   function make_form($list)
   {
-    /*$form = '<form method = "post" action = "set_active.php">';
+    $form = '<form method = "post" action = "set_active.php">';
     foreach($list as $option)
     {
       $form.='<input type = "submit" name = "stream" value = '.$option.'>';
     }
     $form.='</form>';
-    #return $form;
-    */
-    return "test_string";
+    return $form;
+
+    #return "test_string";
   }
 
  ?>
