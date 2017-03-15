2164c5989a4256edd0ceca27d76d6fb70d76cec9
dr-munns <kaspermunns@gmail.com>
2017-03-15 13:02:59 +0000

    make substream options dependant on stream

--- interface.php
+++ interface.php
@@ -2,10 +2,7 @@
   session_start();
   //pull streams
   $streams_all = $_SESSION['all_streams'];
-  //var_dump($streams_all);
-  //$streams = $streams_all[0];
-  //$sub_1 = $streams_all[1];
-  //$sub_2 = $streams_all[2];
+
   include "form_phpmethod.php";
 
  ?>
@@ -13,14 +10,22 @@
   <html>
     <body>
         <?php
+
+        echo make_form($streams_all[0],"set_active.php");
         if(isset($_SESSION['active_stream'])){
           echo "active stream is ". $_SESSION['active_stream'].'<br>';
+          $active_stream_no = array_search(
+            $_SESSION['active_stream'],$streams_all[0]);
+
+            echo make_form_substream($streams_all[$active_stream_no],"set_active.php");
         }
+
         if(isset($_SESSION['active_substream'])){
-          echo "active substream is ". $_SESSION['active_substream'].'<br>';
+          #$active_substream = $_SESSION['active_substream'];
+          echo "active substream is ". $active_substream.'<br>';
         }
-        echo make_form($streams_all[0],"set_active.php");
-        echo make_form_substream($streams_all[1],"set_active.php");
+
+
 
          ?>
     </body>
