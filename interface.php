af070348a05048c37675689d5c3666c161745c23
dr-munns <kaspermunns@gmail.com>
2017-03-15 13:05:12 +0000

    typo
    had commented out a line to just use session var, then used no longer
    defined local var

--- interface.php
+++ interface.php
@@ -22,7 +22,7 @@
 
         if(isset($_SESSION['active_substream'])){
           #$active_substream = $_SESSION['active_substream'];
-          echo "active substream is ". $active_substream.'<br>';
+          echo "active substream is ". $_SESSION['active_substream'].'<br>';
         }
 
 
