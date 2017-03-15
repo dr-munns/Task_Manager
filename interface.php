39abfae227dd40a75263513bfb55cc0f8b53a1e3
dr-munns <kaspermunns@gmail.com>
2017-03-15 12:42:00 +0000

    remove static form

--- interface.php
+++ interface.php
@@ -6,18 +6,12 @@
   //$streams = $streams_all[0];
   //$sub_1 = $streams_all[1];
   //$sub_2 = $streams_all[2];
-  include_once "form_phpmethod.php";
+  include "form_phpmethod.php";
 
  ?>
 <!DOCTYPE HTML>
   <html>
     <body>
-      <form method="post" action="set_active.php" >
-      <br><input type="submit" name="stream" value = "work">
-      <input type="submit" name="stream" value="play"><br>
-
-      </form>
-
         <?php
         if(isset($_SESSION['active_stream'])){
           echo "active stream is ". $_SESSION['active_stream'].'<br>';
