fbd95ac0a65d9fe3f31bb881d89f070796a6b8cc
dr-munns <kaspermunns@gmail.com>
2017-03-15 10:50:04 +0000

    Revert "perhaps this helps"
    This reverts commit ae8df969546f1b50978d78c30c697faf35a83fee.

--- interface.php
+++ interface.php
@@ -31,7 +31,7 @@
         <?php
         echo "pick stream";
         echo make_form($streams_all[0],$action = "set_active.php");
-        if(isset($active_stream))
+        if(!is_null($active_stream))
         {
           echo "active stream is:".$active_stream.'<br>';
           echo "pick substream:";
