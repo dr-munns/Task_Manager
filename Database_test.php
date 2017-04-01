ff564146fd988cf3c9a33ed0bf9d0c7ff54827f0
dr Munns <kaspermunns@gmail.com>
2017-04-01 23:15:45 +0100

    remove a little

--- Database_test.php
+++ Database_test.php
@@ -10,10 +10,11 @@
           $dbname = "Task_Manager";
 
           $conn = new mysqli($servername,$username,$password,$dbname);
+                    /*          
           if ($conn->connect_error){
             die("connoction failed:" . $conn->connect_error)
           }
-                    /*
+
           $SQL = "INSERT INTO
                               Task_Info (name,date,fin)
                               VALUES ('test', DATE '2017-06-1',FALSE)";
