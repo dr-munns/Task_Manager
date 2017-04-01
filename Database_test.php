ce19e1f3ee0f97fc9ba2d18f6464b2592be91e8d
dr Munns <kaspermunns@gmail.com>
2017-04-01 23:22:09 +0100

    see if a connection error is causing it to stop

--- Database_test.php
+++ Database_test.php
@@ -12,7 +12,7 @@
           $conn = new mysqli($servername, $username, $password, $dbname);
 
           if ($conn->connect_error){
-            die("connoction failed:" . $conn->connect_error)
+            echo("connoction failed:" . $conn->connect_error)
           }
 
           $SQL = "INSERT INTO
@@ -24,7 +24,7 @@
           }else {
             echo "error:".$SQL."<br>".$conn->error;
           }
-
+*/
          ?>
        </body>
      </html>
