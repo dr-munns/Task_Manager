bb4d08015535c5084323f83ef0d9fa3bc2370ecc
dr Munns <kaspermunns@gmail.com>
2017-04-02 00:50:21 +0100

    var dump to check what is posted

--- new_task_send.php
+++ new_task_send.php
@@ -1,10 +1,10 @@
 <?php
   $conn = new mysqli("localhost", "tm", "77ML5KFe", "Task_Manager");
-
-  $name = htmlspecialchars($_POST["task name"]);
-  echo $_POST["due date"];
+  Var_dump($_POST);
+  $name = htmlspecialchars($_POST["task_name"]);
   $due = htmlspecialchars($_POST["due date"]);
   $task_id_num = '10';
+  echo $name.$due;
 
   $SQL = "INSERT INTO Task_Info (name,due,fin,task_id) VALUES ($name, DATE $due,FALSE,$task_id_num)";
 
