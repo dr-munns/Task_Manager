15093414f9c9258ad40e461675697cbde5352f62
dr Munns <kaspermunns@gmail.com>
2017-04-01 23:12:13 +0100

    adding back in 2

--- Database_test.php
+++ Database_test.php
@@ -8,11 +8,12 @@
           $username = "tm";
           $password = "77ML5KFe";
           $dbname = "Task_Manager";
-          /*
+
           $conn = new mysqli($servername,$username,$password,$dbname);
           if ($conn->connect_error){
             die("connoction failed:" . $conn->connect_error)
           }
+                    /*
           $SQL = "INSERT INTO
                               Task_Info (name,date,fin)
                               VALUES ('test', DATE '2017-06-1',FALSE)";
