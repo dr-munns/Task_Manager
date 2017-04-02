83eee9dd6838dadb56ee9bf92225b8185cf676fc
dr Munns <kaspermunns@gmail.com>
2017-04-02 01:17:48 +0100

    re added new task form

--- interface.php
+++ interface.php
@@ -23,7 +23,7 @@
         if(isset($_SESSION['active_substream'])){
           #$active_substream = $_SESSION['active_substream'];
           echo "active substream is ". $_SESSION['active_substream'].'<br>';
-          /*echo make_form_newtask();/*/
+          echo make_form_newtask();
         }
 
 
