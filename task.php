be1eacd4de251856625ec7df5e038c14c66be4f4
dr Munns <kaspermunns@gmail.com>
2017-04-02 10:03:03 +0100

    re add sql commands

--- task.php
+++ task.php
@@ -2,13 +2,15 @@
   $conn = new mysqli("localhost", "tm", "77ML5KFe", "Task_Manager");
   if(isset($_POST))
   {
-    print_r(array_keys($_POST));
-    //$id_no = $_POST['done'];
-    //$SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = ".$id_no;
-    //$conn->query($SQL);
+    $id_no= array_keys($_POST)[0];
+    if($_POST[$id_no]='done'){
+      $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = ".$id_no;
+    }
+    //
+    $conn->query($SQL);
   }
 
   $conn->close();
-  //header('Location: interface.php');
-  //exit();
+  header('Location: interface.php');
+  exit();
  ?>
