60156da45f77f5c98b884aff145213ee62a0b5d4
dr Munns <kaspermunns@gmail.com>
2017-04-02 01:52:36 +0100

    removed to find error

--- task.php
+++ task.php
@@ -3,12 +3,12 @@
   if(isset($_POST['done']))
   {
     echo $_POST['done'];
-    $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = $_POST['done']";
+    //$SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = $_POST['done']";
   }
 
-  $conn->query($SQL);
+  //$conn->query($SQL);
 
-  $conn->close();
+  //$conn->close();
   //header('Location: interface.php');
   //exit();
  ?>
