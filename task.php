aeb02776dbabed05f81682957e88b0d218431989
dr Munns <kaspermunns@gmail.com>
2017-04-02 01:54:31 +0100

    set var first

--- task.php
+++ task.php
@@ -2,8 +2,8 @@
   $conn = new mysqli("localhost", "tm", "77ML5KFe", "Task_Manager");
   if(isset($_POST['done']))
   {
-    echo $_POST['done'];
-    $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = $_POST['done']";
+    $id_no = $_POST['done']
+    $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = $id_no";
     echo $SQL;
   }
 
