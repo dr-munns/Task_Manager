e5b3b9e9535a5130f4bbea6ce3bdb49534fff3c5
dr Munns <kaspermunns@gmail.com>
2017-04-02 01:56:33 +0100

    typo

--- task.php
+++ task.php
@@ -2,7 +2,7 @@
   $conn = new mysqli("localhost", "tm", "77ML5KFe", "Task_Manager");
   if(isset($_POST['done']))
   {
-    $id_no = $_POST['done']
+    $id_no = $_POST['done'];
     $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = ".$id_no;
     echo $SQL;
   }
