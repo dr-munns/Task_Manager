29a7d3e0f1976bc57a50fd93a1cbfc797c8411eb
dr Munns <kaspermunns@gmail.com>
2017-04-02 01:51:30 +0100

    check posting correct value

--- task.php
+++ task.php
@@ -2,12 +2,13 @@
   $conn = new mysqli("localhost", "tm", "77ML5KFe", "Task_Manager");
   if(isset($_POST['done']))
   {
+    echo $_POST['done'];
     $SQL = "UPDATE Task_Info SET fin = TRUE WHERE task_id = $_POST['done']";
   }
 
   $conn->query($SQL);
 
   $conn->close();
-  header('Location: interface.php');
-  exit();
+  //header('Location: interface.php');
+  //exit();
  ?>
